b43bf6b21b2a1d89095546ef9708183e48a374c0
Showkat Easin <68397684+ShowkatEasin@users.noreply.github.com>
2022-09-16 23:57:06 +0600

    crud

--- connect.php
+++ connect.php
@@ -1,8 +1,8 @@
 <?php
 
-$con=new mysqli('localhost', 'root', 'pass','crud-operation');
+/* $con=new mysqli('localhost', 'root', 'pass','crud-operation');
 if (!$con){
     die(mysqli_error($con));
-}
+} */
 
 ?>
---
+++ example.php
@@ -0,0 +1,26 @@
+<?php
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<div class="row">
+<div class="box">
+<div class="col-sm-9">
+
+blah blah
+<div class="clearfix"></div>
+</div>
+
+</div>
+
+</div>
+
+</body>
+</html>
